6af4c4b163abee9e31525c3511e5210226adcc7b
Brendan <brendangraff@gmail.com>
2018-03-18 03:21:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ function theBeatlesPlay(musician, instrument)
   
   for (i = 0; i < instrument.length; i++)
   {
-    mArray[i] = `John Lennon plays ${instrument} `;
+    mArray[i] = `${musician} plays ${instrument}`;
   }
   
   return mArray;
+}
+
+function johnLennonFacts(array) {
+  
+  var mArray = {};
+  
+  for( i = 0; i < array.length; i++) {
+    mArray[i] = array[i] + "!!!";
+    
+  }
+  
+  return mArray
+  
 }
